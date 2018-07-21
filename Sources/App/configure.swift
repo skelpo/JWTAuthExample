a61883ecaca4f153e9ad5f535d8237cd3e9dadb5
Karolis Ščerbiakas <k.scerbiakas@gmail.com>
2018-07-21 18:37:17 +0300

    renamed JWTProvider variables for clarity

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,12 +16,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     try services.register(StorageProvider())
     
     /// Adding the JWTProvider for us to validate JWTs
-    try services.register(JWTProvider { n, d in
-        guard let d = d else { throw Abort(.internalServerError, reason: "Could not find environment variable 'JWT_SECRET'", identifier: "missingEnvVar") }
-        
+    try services.register(JWTProvider { _ , pem  in
+        guard let pem = pem else { throw Abort(.internalServerError, reason: "Could not find environment variable 'JWT_SECRET'", identifier: "missingEnvVar") }
         let headers = JWTHeader(alg: "RS256", crit: ["exp", "aud"], kid: "user_manager_kid")
-        return try RSAService(pem: d, header: headers, type: .private, algorithm: .sha512)
-        
+        return try RSAService(pem: pem, header: headers, type: .private, algorithm: .sha512)
     })
 
 
